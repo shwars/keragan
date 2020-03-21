edaadda593cc03bc88e0c6a22c5d0f2d7d147a53
Dmitri Soshnikov <dmitri@soshnikov.com>
2020-03-21 17:20:03 +0300

    Add more options to sign, update readme

--- README.md
+++ README.md
@@ -11,4 +11,55 @@ The simplest way to install is:
 pip install keragan
 ```
 
+### Training GAN from the Command-Line
 
+To start training, you can use the following command-line:
+```python
+python -m keragan.trainer c:\dataset --size 1024 --model_path .\models --samples_path .\samples --latent_dim 100 --epochs 1000
+```
+
+You can find out more about other parameters by calling the program with `--help` option.
+
+Important things to note:
+* `--size` must be power of 2, suitable values are 64, 128, 256, 512 and 1024. Higher resoltions are likely not to give good results.
+* You can use `--lr` to set learning rate, default value is 0.001. Smaller learning rates yield better results, but may significantly increase training time.
+
+### Generating Images
+
+Once you have trained the model, you can use the generator model to produce new random images. To do that from a command line, you can use the following:
+
+```python
+python -m keragan.generate --file ./models/gen_1100.hdf5 --out ./samples --n 100
+```
+
+Use `--help` option to find out more about different options.
+
+### Architecture 
+
+The library is structured around few core classes:
+* `GAN` is used to represent a GAN, with `generator` and `discriminator` fields that define corresponding networks. `GAN` itself is abstract, and any subclass should define `create_generator()` and `create_discriminator()` functions. This class is also responsible for loading/saving networks to disk, and it can also generate sample images using `sample_images` method.
+* `DCGAN` is currently the only subclass, implementing Deep Convolutional GAN.
+* `ImageDataset` is a class defining the process of loading initial dataset from disk, resizing it to specified size, filtering out bad images, etc.
+* `GANTrainer` is responsible to training a GAN, i.e. running epoch loop and periodically storing samples and network weights to disk. 
+
+The actual training code looks like this:
+```python
+    gan = keragan.DCGAN(args)
+    imsrc = keragan.ImageDataset(args)
+    imsrc.load()
+    if args.sample_images:
+        imsrc.sample_images()
+
+    train = keragan.GANTrainer(image_dataset=imsrc,gan=gan,args=args)
+    
+    def callbk(tr):
+        if args.visual_inspection_interval and tr.gan.epoch % args.visual_inspection_interval == 0:
+            res = tr.gan.sample_images(n=2)
+            fig,ax = plt.subplots(1,len(res))
+            for i,v in enumerate(res):
+                ax[i].imshow(v[0])
+            plt.show()
+
+    train.train(callbk)
+
+```
--- keragan/sign.py
+++ keragan/sign.py
@@ -5,24 +5,91 @@ import argparse
 import numpy as np
 import cv2
 import os
+import random
 import glob
+import imutils
 from keragan.utils import imread, imwrite
 
-def imprint(sign,pos,img):
-    overlay_image = sign[..., :3]
-    mask = sign[..., 3:] / 255.0
-    h,w = sign.shape[0],sign.shape[1]
-    size_h,size_w = img.shape[0],img.shape[1]
+from sklearn.cluster import KMeans
+from collections import Counter
+
+
+def percent(value,percents):
+    return int(float(value)*float(percents)/100.0)
+
+
+def get_dominant_color(image, k=4, image_processing_size = None):
+    if image_processing_size is not None:
+        image = cv2.resize(image, image_processing_size, 
+                            interpolation = cv2.INTER_AREA)
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    clt = KMeans(n_clusters = k)
+    labels = clt.fit_predict(image)
+    label_counts = Counter(labels)
+    #subset out most popular centroid
+    dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
+    return dominant_color
+
+def get_av_color(img):
+    m = img[:,:,3]==0
+    ms = np.zeros_like(img[:,:,0:3],dtype=np.bool)
+    ms[:,:,0] = m
+    ms[:,:,1] = m
+    ms[:,:,2] = m
+    ma = np.ma.MaskedArray(img[:,:,0:3].astype(np.float32),ms)
+    return ma.mean(axis=(0,1))
+
+def get_sign(signs,im,args):
+    if len(signs)==1:
+        return signs[0]
+    elif args.stamp_selection == "color":
+        dc = get_dominant_color(im)
+        l = sorted(signs,key=lambda x: -np.sum(np.square(dc-get_av_color(x))))
+        return random.choice(l[0:args.rand_stamp])
+    else:
+        return random.choice(signs)
+
+def get_pos(size_w, size_h, w, h, rcx, rcy, pos):
     if pos == "lr":
-        x,y=size_w-w,size_h-h
+        x,y=size_w-w-rcx,size_h-h-rcy
     elif pos=="ul":
-        x,y = 0,0
+        x,y = rcx,rcy
     elif pos=="ur":
-        x,y=size_w-w,0
+        x,y=size_w-w-rcx,rcy
     elif pos=="ll":
-        x,y=0,size_h-h
+        x,y=rcx,size_h-h-rcy
     else:
         x,y = 0,0
+    return x,y
+
+def imprint(signs,args,img):
+    ra = random.randint(-args.rand_rot,args.rand_rot)
+    size = args.size
+    rand_size = args.rand_size
+    rand_coord = args.rand_coord
+        
+    size_h,size_w = img.shape[0],img.shape[1]
+
+    if args.unit == "percent":
+        size = percent(size_w,size) 
+        rand_size = percent(size,rand_size)
+        rand_coord = percent(size_w,rand_coord)
+
+    rcx = random.randint(0,rand_coord)
+    rcy = random.randint(0,rand_coord)
+    size += random.randint(-rand_size,rand_size)
+
+    pos = args.position.lower()
+    x,y = get_pos(size_w,size_h,size,size,rcx,rcy,pos)
+
+    sign = get_sign(signs,img[y:y+size,x:x+size],args)
+
+    s = cv2.resize(sign,(size,size))
+    s = s if ra==0 else imutils.rotate_bound(s,ra)
+    overlay_image = s[..., :3]
+    mask = s[..., 3:] / 255.0
+    h,w = s.shape[0],s.shape[1]
+    x,y = get_pos(size_w,size_h,w,h,rcx,rcy,pos)
     img[y:y+h, x:x+w] = (1.0 - mask) * img[y:y+h, x:x+w] + mask * overlay_image
     return img
 
@@ -31,35 +98,34 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="KeraGAN Signature Stamper")
 
-    parser.add_argument("--sign",help="Signature file",required=True)
+    parser.add_argument("--sign",help="Signature file or directory",required=True)
     parser.add_argument("--size",help="Size of the signature",default=None,type=int)
-    parser.add_argument("--relsize",help="Relative size as fraction of image size",type=float,default=None)
     parser.add_argument("--position",help="Position: UL,UR,LL,LR",default="LR")
     parser.add_argument("--input",help="Input directory",default=".")
     parser.add_argument("--output",help="Output directory",default=".")
+    parser.add_argument("--rand_coord",help="Coordinate randomization",type=int,default=0)
+    parser.add_argument("--rand_rot",help="Rotation randomization (deg)",type=int,default=0)
+    parser.add_argument("--rand_size",help="Size randomization",type=int,default=0)
+    parser.add_argument("--rand_stamp",help="No of stamp randomization",type=int,default=1)
+    parser.add_argument("--stamp_selection",help="Algorithm of stamp selection [random, color]",default="color",choices=["random","color"])
+    parser.add_argument("--unit",help="Size measurement units: pix or percent",default="pix",choices=["pix","percent"])
+
     args = parser.parse_args()
 
-    print(" + Loading signature...")
-    sign = cv2.imread(args.sign,cv2.IMREAD_UNCHANGED)
-    if args.size:
-        sign = cv2.resize(sign,(args.size,args.size))
+    print(" + Loading signature(s)...")
+    if os.path.isdir(args.sign):
+        sign = [cv2.imread(f,cv2.IMREAD_UNCHANGED) for f in glob.glob(os.path.join(args.sign,'*'))]
+        sign = [x for x in sign if x is not None]
+    else:
+        sign = [cv2.imread(args.sign,cv2.IMREAD_UNCHANGED)]
 
     print(" + Stamping...")
-
     for fn in glob.glob(os.path.join(args.input,"*")):
 #    sz = args.signsize or int(args.size*0.1)
-        print(fn)
-        
-        im = imread(fn)
-        
+        im = imread(fn)        
         if im is None:
             continue
-        if args.relsize:
-            s = int(im.shape[0]*args.relsize)
-            s = cv2.resize(sign,(s,s))
-        else:
-            s = sign
-        res = imprint(s,args.position.lower(),im)
+        res = imprint(sign,args,im)
         imwrite(os.path.join(args.output,os.path.basename(fn)),res)
 
     print(" + Done!")
